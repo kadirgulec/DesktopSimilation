e7f907d367a7dd56a6b9a8b80120b83d307433d2
unknown <devGuelec@gmail.com>
2023-05-08 21:27:44 +0200

    first version

--- desktop.js
+++ desktop.js
@@ -19,14 +19,13 @@ class item {
    
     create(){
         this.componentItems.innerHTML += 
-                                    `<div class="item" id="item${this.itemnr}">
+                                    `<div class="item" id="item${this.itemnr}" onclick="item${this.itemnr}.clicked(${this.itemnr})">
                                         <div class="icon"><img src= "${this.icon}" alt=""></div>
                                         <div class="label">${this.label}</div>
                                     </div>`; 
         this.item = document.getElementById(`item${this.itemnr}`);
         itemIdArray.push(this.itemnr);
-        console.log(itemIdArray);
-        console.log(this.itemnr)
+        
         /* this.item.addEventListener('click', () => this.clicked(itemIdArray[this.itemnr])); */
     
         
@@ -36,22 +35,16 @@ class item {
     }
 
     clicked(number){
+        this.item = document.getElementById(`item${number}`);
         console.log(number);
-        let others = document.querySelector(".item");
-        others.style.backgroundColor = "none";
-        this.item.style.backgroundColor= "blue";
+        for (let i = 0; i < itemIdArray.length; i++){
+            let itemid = document.getElementById(`item${i}`);
+            itemid.style.backgroundColor = "rgba(255, 255, 255, 0)";
+        }
+        
+        this.item.style.backgroundColor= "rgba(56, 56, 190, 0.521)";
     
     }
     
 }
 
-/* let Rechner = new item ("https://images.vexels.com/media/users/3/157498/isolated/preview/3896ab22c743905501eefb8d22607b2f-old-school-rechner-symbol.png", "Rechner");
-Rechner.create();
-let Rechner2 = new item ("https://images.vexels.com/media/users/3/157498/isolated/preview/3896ab22c743905501eefb8d22607b2f-old-school-rechner-symbol.png", "Rechner");
-Rechner2.create();
-let Rechner3 = new item ("https://images.vexels.com/media/users/3/157498/isolated/preview/3896ab22c743905501eefb8d22607b2f-old-school-rechner-symbol.png", "Rechner");
-Rechner3.create();
-let Rechner4 = new item ("https://images.vexels.com/media/users/3/157498/isolated/preview/3896ab22c743905501eefb8d22607b2f-old-school-rechner-symbol.png", "Rechner");
-Rechner4.create();
-
- */
